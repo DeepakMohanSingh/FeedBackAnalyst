4befe79d9b47f2b7cdc8a4bc392719908fce3fd2
Deepak Mohan Singh <46571593+DeepakMohanSingh@users.noreply.github.com>
2021-11-16 14:20:58 +0530

    Update configapi.php

--- configapi.php
+++ configapi.php
@@ -1,9 +1,9 @@
 <?php
 require(__DIR__ . '/vendor/paralleldots/apis/autoload.php');
 # Setting your API key
-set_api_key("oWbiJzj34eCBB3KvfMBgGrAsro3gvB54jYTseART02w");
+set_api_key("<PLEASE PASTE YOUR KEY HERE>");
 # Viewing your API key
 //get_api_key();
-$text_list = "[\"you f**king ass hole\",\"fuck this shit\"]";
+$text_list = "[\"This is the worst product.\",\"Hate this to the core.\"]";
 echo emotion("fuck this shit");
 ?>
