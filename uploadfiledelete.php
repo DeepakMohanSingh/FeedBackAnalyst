3082f4d695015df7494d5c411956a9ed73617af7
Deepak Mohan Singh <46571593+DeepakMohanSingh@users.noreply.github.com>
2023-12-28 12:47:55 +0000

    Delete uploadfiledelete.php

--- uploadfiledelete.php
+++
@@ -1,5 +0,0 @@
-<form action="analysedelete.php" method="POST" enctype="multipart/form-data">
-  <input type="file" name="file" required>
-  <br><br>
-  <input type="submit" name="submit" value="Upload">
-</form>
